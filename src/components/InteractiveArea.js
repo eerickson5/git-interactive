688fc56c6265adb3cac73b9889037035c941e320
Elise Erickson <elisece717@gmail.com>
2024-01-11 21:12:19 -0500

    adds basic params to Repo areas

--- src/components/InteractiveArea.js
+++ src/components/InteractiveArea.js
@@ -4,12 +4,18 @@ import DiffsArea from "./DiffsArea";
 
 export default function InteractiveArea(){
     const [directoryDiffs, setDirectoryDiffs] = useState(["fileA", "fileB", "fileC"])
+    const [stagingDiffs, setStagingDiffs] = useState(["fileD", "fileE", "fileF"])
+    const [localCommits, setLocalCommits] = useState([
+        {sha: "Hi", diffs: ["fileG", "fileH", "fileI"]},
+        {sha: "Bye", diffs: ["fileG", "fileH", "fileI"]},
+    ])
+    const [remoteCommits, setRemoteCommits] = useState([{sha: "Hi", diffs: ["fileG", "fileH", "fileI"]}])
 
     return(
         <div className={"area-container"}>
             <DiffsArea type="working-directory" diffs={directoryDiffs}/>
             <DiffsArea type="staging-area"/>
-            <RepoArea type="commits"/>
+            <RepoArea type="local-repo" versions={localCommits}/>
             <RepoArea type="remote-repo"/>
         </div>
     )
--- src/components/RepoArea.js
+++ src/components/RepoArea.js
@@ -1,18 +1,23 @@
-import React, {useState} from "react";
-import { List, ListItem, ListContent, ListIcon } from "semantic-ui-react";
+import React from "react";
+import { List, ListItem, ListContent, ListIcon, ListHeader } from "semantic-ui-react";
 
-export default function RepoArea({type}){
-
-    const [versions, setVersions] = useState([])
-
+export default function RepoArea({type, versions}){
+    console.log(versions)
     return(
         <div className="area">
             <h2>{type}</h2>
-            <List>
-                {versions.map( v => <ListItem>
-                    <ListIcon name='mail'/>
-                    <ListContent>{v.sha}</ListContent>
-                </ListItem>)}
+            <List celled animated inverted>
+                {!!versions
+                ? versions.map( v => <ListItem key={v}>
+                    <ListIcon name='inbox'/>
+                    <ListContent>
+                        <ListHeader>
+                            {v.sha}
+                        </ListHeader>
+                        {v.diffs.map(d => <p key={d}>{d}</p>)}
+                    </ListContent>
+                </ListItem>)
+                : <p>None</p>}
             </List>
         </div>
     )
