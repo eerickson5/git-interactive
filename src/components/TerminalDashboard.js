5bc2022c9b983cf8d57963058df6072a5ef7e130
Elise Erickson <elisece717@gmail.com>
2024-01-13 10:23:52 -0500

    connects terminal buttons to gitcontext functions

--- src/components/TerminalDashboard.js
+++ src/components/TerminalDashboard.js
@@ -5,15 +5,17 @@ import { GitContext } from "./GitProvider";
 
 export default function TerminalDashboard({consoleOutput}){
 
+    const { moveFilesToStaging, commitFilesToLocal, commitFilesToRemote } = useContext(GitContext)
+
     return (
         <Segment raised inverted className="terminal-dashboard">
 
             <h2>Terminal Dashboard</h2>
             <FileChanger/>
             <div className="buttons-container">
-                <Button color='purple'>git add</Button>
-                <Button color='purple'>git commit</Button>
-                <Button color='purple'>git push</Button>
+                <Button color='purple' onClick={moveFilesToStaging}>git add</Button>
+                <Button color='purple' onClick={commitFilesToLocal}>git commit</Button>
+                <Button color='purple' onClick={commitFilesToRemote}>git push</Button>
             </div>
             <h3>[console] No files to add.</h3>
         </Segment>
