a2f2d5d31858f7180419e598e8718d3e3aef9551
Elise Erickson <elisece717@gmail.com>
2024-01-13 11:14:36 -0500

    adds logo to header

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,7 @@ import './App.css';
 import InteractiveArea from './InteractiveArea';
 import TerminalDashboard from './TerminalDashboard';
 import {GitProvider} from './GitProvider';
-
-const consoleOutputs = [
-  "1 file changed. Your branch is ahead of 'origin/main' by 1 commit.",
-]
+import {Icon} from 'semantic-ui-react'
 
 function App() {
   return (
@@ -13,6 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>git-interactive</h1>
+          <Icon name='github'/>
         </header>
 
         <InteractiveArea/>
