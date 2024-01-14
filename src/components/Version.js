229cfae1bfefd7c9f92543138224263ba1052e38
Elise Erickson <elisece717@gmail.com>
2024-01-14 14:08:49 -0500

    shows commit messages in repos

--- src/components/Version.js
+++ src/components/Version.js
@@ -8,7 +8,7 @@ export default function Version({version}){
             <ListIcon name='inbox'/>
             <ListContent >
                 <ListHeader>
-                    {version.sha}
+                    {`[${version.sha}] ${version.message}`}
                 </ListHeader>
                 <p>{version.diffs.join(", ")}</p>
             </ListContent>
