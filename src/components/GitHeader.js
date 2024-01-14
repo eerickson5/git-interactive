dac6a20f858ea6f67c6203e1a6733db0385e09a6
Elise Erickson <elisece717@gmail.com>
2024-01-14 14:14:29 -0500

    styles header

--- src/components/GitHeader.js
+++ src/components/GitHeader.js
@@ -4,10 +4,14 @@ import {Icon} from 'semantic-ui-react'
 
 export default function GitHeader(){
     return(
-        <div>
-            <h1>git-interactive</h1>
+        <div className="header-container">
+            <div className="horizontal">
+                <h1>git-interactive</h1>
+                <Icon name='github'/>
+            </div>
+            
+
             <h4>a visual tool for understanding version control with git.</h4>
-            <Icon name='github'/>
         </div>
     )
 }
